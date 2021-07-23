1fdf664f82628cfb58cd81c6b63edee2576fbef5
Steven Hughes <thughes34@gatech.edu>
2021-07-22 22:31:35 -0400

    Basic requirements added

--- bin/index.js
+++ bin/index.js
@@ -2,10 +2,13 @@
 
 const { Command } = require('commander');
 const axios = require('axios').default;
+const util = require('util')
 
+const pullRequestEvent = 'PullRequestEvent'
 const program = new Command();
 program.version('0.0.1');
 let url
+let capturedEvents = []
 
 program
     .requiredOption('-o --organization <org>', 'the name of the org to analyze', formatUrl)
@@ -16,18 +19,54 @@ const options = program.opts();
 analyze(url)
 
 function formatUrl(optionValue, _) {
-    url = `https://api.github.com/orgs/${optionValue}`
+    url = `https://api.github.com/orgs/${optionValue}/events`
     return optionValue
+}
+
+function parseEvents(response) {
+    console.log('Got a response!!')
+    console.log(response)
+    for (const ev of response.data) {
+        if (ev.type === pullRequestEvent) {
+            capturedEvents.push(ev)
+        }
+    }
+    if (response.headers['link']){
+        let links = response.headers['link'].split(', ')
+        let nextUrl
+        for (const link of links){
+            console.log(`parsing link ${link}`)
+            if(link.indexOf('rel="next"') !== -1){
+                nextUrl = link.split('>')[0].split('<')[1]
+                break
+            }
+        }
+        return nextUrl
+    }
+    return
 }
 
 function analyze(url) {
     console.log(`Got a url: ${url}!!!`)
     axios.get(url)
-        .then(function (resp){
-            console.log('Got a response!!')
-            console.log(resp)
+        .then(function(response){
+            console.log('After request...')
+            let next = parseEvents(response)
+            console.log(`next: ${next}, !!next: ${!!next}`)
+            if (!!next){
+                analyze(next)
+            } else {
+                finalize()
+            }
         })
         .catch(function (error) {
             console.log(`Unable to complete request: ${error}`);
-          })
+        })
+}
+
+function finalize(){
+    console.log(`Analysis complete, found ${capturedEvents.length} pull requests`)
+    for (const ev of capturedEvents) {
+        console.log(ev)
+    }
 }
